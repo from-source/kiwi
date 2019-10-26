5d14f4f00088f176a7ee6c8f4046d255e6ee6c14
Arek Torczuk <torczuk@gmail.com>
2019-10-27 01:10:13 +0800

    Added basic operators for Collections- contain and List - have1st have2nd

--- kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/collection/CollectionShould.kt
+++ kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/collection/CollectionShould.kt
@@ -5,20 +5,28 @@ open class CollectionShould<T, R : CollectionShould<T, R>>(open val actual: Coll
     open infix fun haveSize(size: Int): R {
         val hasSize = actual.size == size
         val message = "Expected size: $size, actual: ${actual.size}"
-        assert(hasSize) { -> message }
+        assert(hasSize) { message }
         return this as R
     }
 
     open infix fun contain(element: T): R {
-
+        val contains = actual.contains(element)
+        val message = "$actual should contain $element"
+        assert(contains) { message }
         return this as R
     }
-//
-//    open fun containAll(vararg  elements: T): R {
-//        return this as R
-//    }
+
+    open fun contain(vararg elements: T): R {
+        elements.forEach {
+            val message = "$actual should contain $it"
+            assert(actual.contains(it)) { message }
+        }
+        return this as R
+    }
 
     open fun beEmpty(): R {
+        val message = "$actual should be empty"
+        assert(actual.isEmpty()) { message }
         return this as R
     }
 }
@@ -26,13 +34,19 @@ open class CollectionShould<T, R : CollectionShould<T, R>>(open val actual: Coll
 
 open class ListShould<T>(override val actual: List<T>) : CollectionShould<T, ListShould<T>>(actual) {
 
-//    open infix fun have1st(element: T): ListShould<T> {
-//        return this
-//    }
-//
-//    open infix fun have2nd(element: T): ListShould<T> {
-//        return this
-//    }
+    open infix fun have1st(element: T): ListShould<T> {
+        val message = "$actual should have first element to be $element"
+        assert(actual.isNotEmpty()) { message }
+        assert(actual.first() == element) { message }
+        return this
+    }
+
+    open infix fun have2nd(element: T): ListShould<T> {
+        val message = "$actual should have second element to be $element"
+        assert(actual.size >= 2) { message }
+        assert(actual[1] == element) { message }
+        return this
+    }
 
 //    open fun first(): T {
 //        return actual.first()
--- kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/collection/ListShouldTest.kt
+++ kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/collection/ListShouldTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 class ListShouldTest {
 
     @Test
-    fun `should throw AssertionError when list does not have expected size`() {
+    fun `should fail when list does not have expected size`() {
         val numbers = listOf(1, 2, 3)
 
         runCatching {
@@ -18,10 +18,122 @@ class ListShouldTest {
     }
 
     @Test
-    fun `should guarantee list have`() {
+    fun `should guarantee list have size`() {
         val numbers = listOf(1, 2, 3)
 
         numbers.should() haveSize 3
+    }
+
+    @Test
+    fun `should fail when list does not contain element`() {
+        val numbers = listOf(1, 2, 3)
+
+        runCatching {
+            numbers.should() contain 4
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[1, 2, 3] should contain 4")
+    }
+
+    @Test
+    fun `should guarantee that list contains element`() {
+        val numbers = listOf(1, 2, 3)
+
+        numbers.should() contain 3
+    }
+
+    @Test
+    fun `should fail when list is not empty`() {
+        val numbers = listOf(1, 2, 3)
+
+        runCatching {
+            numbers.should().beEmpty()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[1, 2, 3] should be empty")
+    }
+
+    @Test
+    fun `should guarantee that list is empty`() {
+        val numbers = emptyList<Number>()
+
+        numbers.should().beEmpty()
+    }
+
+    @Test
+    fun `should fail when list does not contain all elements in order`() {
+        val numbers = listOf(1, 2, 3)
+
+        runCatching {
+            numbers.should().contain(3, 4)
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[1, 2, 3] should contain 4")
+    }
+
+    @Test
+    fun `should guarantee that list contains all elements`() {
+        val numbers = listOf(1, 2, 3)
+
+        numbers.should().contain(1, 3)
+    }
+
+    @Test
+    fun `should fail when non empty list does not have 1st element as`() {
+        val numbers = listOf(1, 2, 3)
+
+        runCatching {
+            numbers.should() have1st 2
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[1, 2, 3] should have first element to be 2")
+    }
+
+    @Test
+    fun `should guarantee that list contains have element to be equal at first position`() {
+        val numbers = listOf(1, 2, 3)
+
+        numbers.should() have1st 1
+    }
+
+    @Test
+    fun `should fail when empty list expected to have element at first position`() {
+        val numbers = emptyList<Number>()
+
+        runCatching {
+            numbers.should() have1st 2
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[] should have first element to be 2")
+    }
+
+    @Test
+    fun `should fail when non empty list does not have 2nd element as`() {
+        val numbers = listOf(1, 2, 3)
+
+        runCatching {
+            numbers.should() have2nd 1
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[1, 2, 3] should have second element to be 1")
+    }
+
+    @Test
+    fun `should guarantee that list contains have element to be equal at second position`() {
+        val numbers = listOf(1, 2, 3)
+
+        numbers.should() have2nd 2
+    }
+
+    @Test
+    fun `should fail when empty list expected to have element at second position`() {
+        val numbers = emptyList<Number>()
+
+        runCatching {
+            numbers.should() have2nd 2
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("[] should have second element to be 2")
     }
 }
 
