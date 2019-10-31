2c9545063eb5708ec4d069b25dbeb26a1df858b8
Arek Torczuk <torczuk@gmail.com>
2019-10-31 21:39:11 +0800

    Introduce NumberShould interface

--- kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/Kiwi.kt
+++ kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/Kiwi.kt
@@ -1,9 +1,15 @@
 package io.github.fromsource.kiwi.core
 
 import io.github.fromsource.kiwi.core.collection.ListShould
-import io.github.fromsource.kiwi.core.number.IntShould
+import io.github.fromsource.kiwi.core.number.*
 import io.github.fromsource.kiwi.core.string.StringShould
 
 fun <T> List<T>.should() = ListShould(this)
+
 fun Int.should() = IntShould(this)
+fun Short.should() = ShortShould(this)
+fun Long.should() = LongShould(this)
+fun Float.should() = FloatShould(this)
+fun Double.should() = DoubleShould(this)
+
 fun String.should() = StringShould(this)
--- kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/number/NumbersShould.kt
+++ kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/number/NumbersShould.kt
@@ -3,22 +3,24 @@ package io.github.fromsource.kiwi.core.number
 import io.github.fromsource.kiwi.core.BeComparable
 import io.github.fromsource.kiwi.core.BeEqual
 
-class IntShould(private val actual: Int) : BeEqual<Int, IntShould>, BeComparable<Int, IntShould> {
+interface NumbersShould<T : Comparable<T>> : BeEqual<T, NumbersShould<T>>, BeComparable<T, NumbersShould<T>>
+
+class IntShould(private val actual: Int) : NumbersShould<Int> {
     override fun actual(): Int = actual
 }
 
-class LongShould(private val actual: Long) : BeEqual<Long, LongShould>, BeComparable<Long, LongShould> {
+class LongShould(private val actual: Long) : NumbersShould<Long> {
     override fun actual(): Long = actual
 }
 
-class DoubleShould(private val actual: Double) : BeEqual<Double, DoubleShould>, BeComparable<Double, DoubleShould> {
+class DoubleShould(private val actual: Double) : NumbersShould<Double> {
     override fun actual(): Double = actual
 }
 
-class FloatShould(private val actual: Float) : BeEqual<Float, FloatShould>, BeComparable<Float, FloatShould> {
+class FloatShould(private val actual: Float) : NumbersShould<Float> {
     override fun actual(): Float = actual
 }
 
-class ShortShould(private val actual: Short) : BeEqual<Short, ShortShould>, BeComparable<Short, ShortShould> {
+class ShortShould(private val actual: Short) : NumbersShould<Short> {
     override fun actual(): Short = actual
 }
