d3cbbe7e0265100e157d035becdf40297edf638d
Arek Torczuk <torczuk@gmail.com>
2019-11-01 19:55:56 +0800

    Add Maps operators. Extract ListShould MapShould SetShould to separate files

---
+++ kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/collection/SetShould.kt
@@ -0,0 +1,3 @@
+package io.github.fromsource.kiwi.core.collection
+
+open class SetShould<T>(override val actual: Set<T>) : CollectionShould<T, SetShould<T>>(actual)
--- kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/collection/MapShouldTest.kt
+++ kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/collection/MapShouldTest.kt
@@ -7,6 +7,42 @@ import org.junit.jupiter.api.Test
 class MapShouldTest {
 
     @Test
+    fun `should fail when map is not empty`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should().beEmpty()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should be empty")
+    }
+
+    @Test
+    fun `should guarantee map is empty`() {
+        val letters = emptyMap<String, Int>()
+
+        letters.should().beEmpty()
+    }
+
+    @Test
+    fun `should fail when map has not proper size`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should() haveSize 3
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should have size 3")
+    }
+
+    @Test
+    fun `should guarantee map has proper size`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        letters.should() haveSize 2
+    }
+
+    @Test
     fun `should fail when maps are not equaled`() {
         val letters = mapOf("a" to 1, "b" to 2)
 
@@ -22,5 +58,77 @@ class MapShouldTest {
         val letters = mapOf("a" to 1, "b" to 2)
 
         letters.should() beEqual mapOf("b" to 2, "a" to 1)
+    }
+
+    @Test
+    fun `should fail when map does not contain key`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should() containKey "c"
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should contain key 'c'")
+    }
+
+    @Test
+    fun `should guarantee map contain key`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        letters.should() containKey "a"
+    }
+
+    @Test
+    fun `should fail when map does not contain value`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should() containValue 0
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should contain value '0'")
+    }
+
+    @Test
+    fun `should guarantee map contain value`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        letters.should() containValue 2
+    }
+
+    @Test
+    fun `should fail when map does not match any predicate`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should() matchAny { (key, _) -> key == "c" }
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should match any predicate")
+    }
+
+    @Test
+    fun `should guarantee map match any predicate`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        letters.should() matchAny { (key, _) -> key == "a" }
+    }
+
+    @Test
+    fun `should fail when map does not match all predicate`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        runCatching {
+            letters.should() matchAll { (key, _) -> key == "c" }
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("{a=1, b=2} should match all predicate")
+    }
+
+    @Test
+    fun `should guarantee map match all predicate`() {
+        val letters = mapOf("a" to 1, "b" to 2)
+
+        letters.should() matchAll { (_, value) -> value > 0 }
     }
 }
