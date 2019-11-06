961e8f8ea6a08e679e7e19eb3eb6729e0b9b3f2a
Denise <denise.lim.sh@gmail.com>
2019-11-06 17:18:54 +0800

    #4 Standardize path fed to File in tests

--- kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/file/PathShouldTest.kt
+++ kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/file/PathShouldTest.kt
@@ -8,50 +8,50 @@ class PathShouldTest {
 
     @Test
     fun `should be able to verify if path exist`() {
-        File("./src/test/resources/sample.txt").toPath().should().exist()
+        File("src/test/resources/sample.txt").toPath().should().exist()
     }
 
     @Test
     fun `should fail if path does not exist`() {
-        val path = File("./src/test/resources/non_existent.txt").toPath()
+        val path = File("src/test/resources/non_existent.txt").toPath()
 
         runCatching {
             path.should().exist()
         }.should()
                 .beFailure(AssertionError::class)
-                .haveFailureMessage("Path './src/test/resources/non_existent.txt' does not exist")
+                .haveFailureMessage("Path 'src/test/resources/non_existent.txt' does not exist")
     }
 
     @Test
     fun `should be able to verify if path points to a directory`() {
-        File("./src/test/resources").toPath().should().beADirectory()
+        File("src/test/resources").toPath().should().beADirectory()
     }
 
     @Test
     fun `should fail if path does not point to a directory`() {
-        val path = File("./src/test/resources/sample.txt").toPath()
+        val path = File("src/test/resources/sample.txt").toPath()
 
         runCatching {
             path.should().beADirectory()
         }.should()
                 .beFailure(AssertionError::class)
-                .haveFailureMessage("Path './src/test/resources/sample.txt' is not a directory")
+                .haveFailureMessage("Path 'src/test/resources/sample.txt' is not a directory")
     }
 
     @Test
     fun `should be able to verify if path points to a file`() {
-        File("./src/test/resources/sample.txt").toPath().should().beAFile()
+        File("src/test/resources/sample.txt").toPath().should().beAFile()
     }
 
     @Test
     fun `should fail if path does not point to a file`() {
-        val path = File("./src/test/resources").toPath()
+        val path = File("src/test/resources").toPath()
 
         runCatching {
             path.should().beAFile()
         }.should()
                 .beFailure(AssertionError::class)
-                .haveFailureMessage("Path './src/test/resources' is not a file")
+                .haveFailureMessage("Path 'src/test/resources' is not a file")
     }
 
     @Test
@@ -62,12 +62,12 @@ class PathShouldTest {
 
     @Test
     fun `should fail if path is not absolute`() {
-        val path = File("./src/test/resources").toPath()
+        val path = File("src/test/resources").toPath()
         runCatching {
             path.should().beAbsolute()
         }.should()
                 .beFailure(AssertionError::class)
-                .haveFailureMessage("Path './src/test/resources' is not absolute")
+                .haveFailureMessage("Path 'src/test/resources' is not absolute")
     }
 
     @Test
