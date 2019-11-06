7ee9216f483190e0586bff6669f353e66383f6d8
Arek Torczuk <torczuk@gmail.com>
2019-11-06 19:30:06 +0800

    #3 Add comparision between different type of Numbers for Int

--- kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/number/NumberShould.kt
+++ kiwi-core/src/main/kotlin/io/github/fromsource/kiwi/core/number/NumberShould.kt
@@ -72,61 +72,61 @@ class ByteShould(private val actual: Byte) : NumberShould<Byte> {
 class ShortShould(private val actual: Short) : NumberShould<Short> {
     override fun actual(): Short = actual
 
-    infix fun beLessThan(expected: Byte): NumberShould<Short> {
+    infix fun beLessThan(expected: Byte): ShortShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Int): NumberShould<Short> {
+    infix fun beLessThan(expected: Int): ShortShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Long): NumberShould<Short> {
+    infix fun beLessThan(expected: Long): ShortShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Float): NumberShould<Short> {
+    infix fun beLessThan(expected: Float): ShortShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Double): NumberShould<Short> {
+    infix fun beLessThan(expected: Double): ShortShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beGreaterThan(expected: Byte): NumberShould<Short> {
+    infix fun beGreaterThan(expected: Byte): ShortShould {
         val message = "${actual()} should be > $expected"
         assert(actual() > expected) { message }
         return this
     }
 
-    infix fun beGreaterThan(expected: Int): NumberShould<Short> {
+    infix fun beGreaterThan(expected: Int): ShortShould {
         val message = "${actual()} should be > $expected"
         assert(actual() > expected) { message }
         return this
     }
 
-    infix fun beGreaterThan(expected: Long): NumberShould<Short> {
+    infix fun beGreaterThan(expected: Long): ShortShould {
         val message = "${actual()} should be > $expected"
         assert(actual() > expected) { message }
         return this
     }
 
-    infix fun beGreaterThan(expected: Float): NumberShould<Short> {
+    infix fun beGreaterThan(expected: Float): ShortShould {
         val message = "${actual()} should be > $expected"
         assert(actual() > expected) { message }
         return this
     }
 
-    infix fun beGreaterThan(expected: Double): NumberShould<Short> {
+    infix fun beGreaterThan(expected: Double): ShortShould {
         val message = "${actual()} should be > $expected"
         assert(actual() > expected) { message }
         return this
@@ -136,33 +136,63 @@ class ShortShould(private val actual: Short) : NumberShould<Short> {
 class IntShould(private val actual: Int) : NumberShould<Int> {
     override fun actual(): Int = actual
 
-    infix fun beLessThan(expected: Byte): NumberShould<Int> {
+    infix fun beLessThan(expected: Byte): IntShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Short): NumberShould<Int> {
+    infix fun beLessThan(expected: Short): IntShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Long): NumberShould<Int> {
+    infix fun beLessThan(expected: Long): IntShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Float): NumberShould<Int> {
+    infix fun beLessThan(expected: Float): IntShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
         return this
     }
 
-    infix fun beLessThan(expected: Double): NumberShould<Int> {
+    infix fun beLessThan(expected: Double): IntShould {
         val message = "${actual()} should be < $expected"
         assert(actual() < expected) { message }
+        return this
+    }
+
+    infix fun beGreaterThan(expected: Byte): IntShould {
+        val message = "${actual()} should be > $expected"
+        assert(actual() > expected) { message }
+        return this
+    }
+
+    infix fun beGreaterThan(expected: Short): IntShould {
+        val message = "${actual()} should be > $expected"
+        assert(actual() > expected) { message }
+        return this
+    }
+
+    infix fun beGreaterThan(expected: Long): IntShould {
+        val message = "${actual()} should be > $expected"
+        assert(actual() > expected) { message }
+        return this
+    }
+
+    infix fun beGreaterThan(expected: Float): IntShould {
+        val message = "${actual()} should be > $expected"
+        assert(actual() > expected) { message }
+        return this
+    }
+
+    infix fun beGreaterThan(expected: Double): IntShould {
+        val message = "${actual()} should be > $expected"
+        assert(actual() > expected) { message }
         return this
     }
 }
--- kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/number/IntShouldTest.kt
+++ kiwi-core/src/test/kotlin/io/github/fromsource/kiwi/core/number/IntShouldTest.kt
@@ -119,5 +119,75 @@ class IntShouldTest {
     fun `should guarantee than number is greater than`() {
         more.should() beGreaterThan less
     }
+
+    @Test
+    fun `should fail when number is not greater than byte`() {
+        runCatching {
+            less.should() beGreaterThan more.toByte()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("$less should be > ${more.toByte()}")
+    }
+
+    @Test
+    fun `should guarantee than number is greater than byte`() {
+        more.should() beGreaterThan less.toByte()
+    }
+
+    @Test
+    fun `should fail when number is not greater than short`() {
+        runCatching {
+            less.should() beGreaterThan more.toShort()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("$less should be > ${more.toShort()}")
+    }
+
+    @Test
+    fun `should guarantee than number is greater than short`() {
+        more.should() beGreaterThan less.toShort()
+    }
+
+    @Test
+    fun `should fail when number is not greater than long`() {
+        runCatching {
+            less.should() beGreaterThan more.toLong()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("$less should be > ${more.toLong()}")
+    }
+
+    @Test
+    fun `should guarantee than number is greater than long`() {
+        more.should() beGreaterThan less.toLong()
+    }
+
+    @Test
+    fun `should fail when number is not greater than float`() {
+        runCatching {
+            less.should() beGreaterThan more.toFloat()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("$less should be > ${more.toFloat()}")
+    }
+
+    @Test
+    fun `should guarantee than number is greater than float`() {
+        more.should() beGreaterThan less.toFloat()
+    }
+
+    @Test
+    fun `should fail when number is not greater than double`() {
+        runCatching {
+            less.should() beGreaterThan more.toDouble()
+        }.should()
+                .beFailure(AssertionError::class)
+                .haveFailureMessage("$less should be > ${more.toDouble()}")
+    }
+
+    @Test
+    fun `should guarantee than number is greater than double`() {
+        more.should() beGreaterThan less.toDouble()
+    }
 }
 
