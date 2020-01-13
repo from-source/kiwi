17eef80b77cda39588f883d009ae8e50116c84d0
Sergey <sergey.bukharov@thoughtworks.com>
2020-01-13 11:40:06 +0800

    #5 Add boolean support
    Allows to assert a boolean value against another Boolean
    Allows to assert boolean against isTrue() or isFalse()
    Allows to assert boolean against another boolean with infix notation

--- kiwi-core/src/commonMain/kotlin/io/fromsource/kiwi/core/Kiwi.kt
+++ kiwi-core/src/commonMain/kotlin/io/fromsource/kiwi/core/Kiwi.kt
@@ -1,9 +1,19 @@
 package io.fromsource.kiwi.core
 
+import io.fromsource.kiwi.core.bool.BooleanShould
 import io.fromsource.kiwi.core.collection.ListShould
 import io.fromsource.kiwi.core.collection.MapShould
 import io.fromsource.kiwi.core.collection.SetShould
-import io.fromsource.kiwi.core.number.*
+import io.fromsource.kiwi.core.number.ByteShould
+import io.fromsource.kiwi.core.number.DoubleShould
+import io.fromsource.kiwi.core.number.FloatShould
+import io.fromsource.kiwi.core.number.IntShould
+import io.fromsource.kiwi.core.number.LongShould
+import io.fromsource.kiwi.core.number.ShortShould
+import io.fromsource.kiwi.core.number.UByteShould
+import io.fromsource.kiwi.core.number.UIntShould
+import io.fromsource.kiwi.core.number.ULongShould
+import io.fromsource.kiwi.core.number.UShortShould
 import io.fromsource.kiwi.core.string.StringShould
 
 fun <T> List<T>.should() = ListShould(this)
@@ -17,6 +27,10 @@ fun Int.should() = IntShould(this)
 fun Long.should() = LongShould(this)
 fun Float.should() = FloatShould(this)
 fun Double.should() = DoubleShould(this)
+fun Boolean.should() = BooleanShould(this)
+fun String.should() = StringShould(this)
+fun <T> Result<T>.should(): ResultShould<T> = ResultShould(this)
+fun <A, B> Pair<A, B>.should(): PairShould<A, B> = PairShould(this)
 
 /**
  * Temporary rename should() to _should() for unsigned types
@@ -30,12 +44,6 @@ internal fun UShort._should() = UShortShould(this)
 internal fun UInt._should() = UIntShould(this)
 @ExperimentalUnsignedTypes
 internal fun ULong._should() = ULongShould(this)
-
-fun String.should() = StringShould(this)
-
-fun <T> Result<T>.should(): ResultShould<T> = ResultShould(this)
-
-fun <A, B> Pair<A, B>.should(): PairShould<A, B> = PairShould(this)
 
 fun assert(condition: Boolean, lazyMessage: () -> String) {
     if(!condition) throw AssertionError(lazyMessage())
